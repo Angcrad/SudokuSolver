73da5999679dbeb09b6693b236cd7d5e96f437eb
Angcrad <49344236+Angcrad@users.noreply.github.com>
2025-05-25 19:11:37 -0600

    Initial Commit

---
+++ SudokuSolver/GridSquare.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver
+{
+	public class GridSquare : Button
+	{
+		public int RowID { get; set; }
+		public int ColID { get; set; }
+
+		public GridSquare(int row, int col, int value)
+		{
+			RowID = row;
+			ColID = col;
+			this.Text = value.ToString();
+			this.Width = 40;
+			this.Height = 40;
+			this.Margin = new Padding(1);
+			this.Font = new Font("Segoe UI", 12, FontStyle.Bold);
+		}
+	}
+}
